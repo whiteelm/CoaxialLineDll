faa543c29bab1e201ad30a931a4c60d72fe75a9d
thiswhitenike@gmail.com <thiswhitenike@gmail.com>
2022-07-28 09:00:25 +0700

    Удалено лишнее

--- Coax Programms/Coaxial v2/test5main/Debug/count0__genmod.f90
+++
@@ -1,9 +0,0 @@
-        !COMPILER-GENERATED INTERFACE MODULE: Wed May 05 20:37:30 2021
-        ! This source file is for reference only and may not completely
-        ! represent the generated interface used by the compiler.
-        MODULE COUNT0__genmod
-          INTERFACE 
-            SUBROUTINE COUNT0
-            END SUBROUTINE COUNT0
-          END INTERFACE 
-        END MODULE COUNT0__genmod
