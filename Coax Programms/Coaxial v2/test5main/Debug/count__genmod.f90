37accb8b06dc465c238ab1e3d97f3c2eeb5b88af
thiswhitenike@gmail.com <thiswhitenike@gmail.com>
2022-07-28 08:55:43 +0700

    Добавлено решение

---
+++ Coax Programms/Coaxial v2/test5main/Debug/count__genmod.f90
@@ -0,0 +1,9 @@
+        !COMPILER-GENERATED INTERFACE MODULE: Wed May 05 20:37:30 2021
+        ! This source file is for reference only and may not completely
+        ! represent the generated interface used by the compiler.
+        MODULE COUNT__genmod
+          INTERFACE 
+            SUBROUTINE COUNT
+            END SUBROUTINE COUNT
+          END INTERFACE 
+        END MODULE COUNT__genmod
---
+++ Coax Programms/Coaxial v2/test5main/Debug/dminv__genmod.f90
@@ -0,0 +1,12 @@
+        !COMPILER-GENERATED INTERFACE MODULE: Wed Apr 14 15:23:55 2021
+        ! This source file is for reference only and may not completely
+        ! represent the generated interface used by the compiler.
+        MODULE DMINV__genmod
+          INTERFACE 
+            SUBROUTINE DMINV(A,N,D)
+              REAL(KIND=8) :: A(1)
+              INTEGER(KIND=4) :: N
+              REAL(KIND=8) :: D
+            END SUBROUTINE DMINV
+          END INTERFACE 
+        END MODULE DMINV__genmod
